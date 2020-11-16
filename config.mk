aaa41a7aae1d32dfe23ea58f8276c3efaab13b1a
mendess <pedro.mendes.26@gmail.com>
2020-11-16 16:22:23 +0000

    center, border, color

--- config.mk
+++ config.mk
@@ -18,14 +18,16 @@ FREETYPEINC = /usr/include/freetype2
 # OpenBSD (uncomment)
 #FREETYPEINC = $(X11INC)/freetype2
 
+NUM_THREADS = $(shell nproc)
+
 # includes and libs
 INCS = -I$(X11INC) -I$(FREETYPEINC)
 LIBS = -L$(X11LIB) -lX11 $(XINERAMALIBS) $(FREETYPELIBS)
 
 # flags
-CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"$(VERSION)\" $(XINERAMAFLAGS) -pthread
-CFLAGS   = -std=c99 -pedantic -Wall -Ofast $(INCS) $(CPPFLAGS)
-LDFLAGS  = $(LIBS)
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"$(VERSION)\" $(XINERAMAFLAGS)
+CFLAGS   = -std=c99 -pedantic -Wall -Ofast $(INCS) $(CPPFLAGS) -DNUM_THREADS=$(NUM_THREADS)
+LDFLAGS  = $(LIBS) -pthread
 
 # compiler and linker
 CC = cc
